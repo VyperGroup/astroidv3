3c49d51ece75d9f5627dc30549714ad2aabdebd7
FatGrizzly <66728880+FatGrizzly@users.noreply.github.com>
2023-09-04 19:20:38 +0530

    Update index.js
    Signed-off-by: FatGrizzly <66728880+FatGrizzly@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
-//import createBareServer from "@tomphttp/bare-server-node";
 import { createBareServer } from '@tomphttp/bare-server-node';
 import express from "express";
 import { createServer } from "node:http";
-// import { publicPath } from "ultraviolet-static";
+import { SocksProxyAgent } from 'socks-proxy-agent';
+const socksProxyAgent = new SocksProxyAgent('socks://localhost:40000');
 import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
 import { join } from "node:path";
 import { hostname } from "node:os";
 import dotenv from 'dotenv';
-
 import { fileURLToPath } from "url";
-
 const publicPath = fileURLToPath(new URL("./static/", import.meta.url));
-
-const bare = createBareServer("/bare/");
+const bare = createBareServer('/bare/', {
+        httpAgent: socksProxyAgent,
+        httpsAgent: socksProxyAgent,
+});
 const app = express();
 dotenv.config();
-
-// Load our publicPath first and prioritize it over UV.
 app.use(express.static(publicPath));
-// Load vendor files last.
-// The vendor's uv.config.js won't conflict with our uv.config.js inside the publicPath directory.
 app.use("/uv/", express.static(uvPath));
-
-// Error for everything else
-
 const server = createServer();
-
 server.on("request", (req, res) => {
   if (bare.shouldRoute(req)) {
     bare.routeRequest(req, res);
@@ -33,7 +25,6 @@ server.on("request", (req, res) => {
     app(req, res);
   }
 });
-
 server.on("upgrade", (req, socket, head) => {
   if (bare.shouldRoute(req)) {
     bare.routeUpgrade(req, socket, head);
@@ -41,22 +32,9 @@ server.on("upgrade", (req, socket, head) => {
     socket.end();
   }
 });
-
 const port = process.env.PORT || 3300;
-
 server.on("listening", () => {
-  const address = server.address();
-
-  // by default we are listening on 0.0.0.0 (every interface)
-  // we just need to list a few
-  console.log("Listening on:");
-  console.log(`\thttp://localhost:${address.port}`);
-  console.log(`\thttp://${hostname()}:${address.port}`);
-  console.log(
-    `\thttp://${
-      address.family === "IPv6" ? `[${address.address}]` : address.address
-    }:${address.port}`
-  );
+  console.log('UP')
 });
 
 server.listen({
