efa1c31a9693d5f472547773835ed2d84987e4b4
FatGrizzly <66728880+FatGrizzly@users.noreply.github.com>
2023-10-11 19:00:49 +0530

    Create bare.js
    Signed-off-by: FatGrizzly <66728880+FatGrizzly@users.noreply.github.com>

---
+++ bare.js
@@ -0,0 +1,31 @@
+const http = require('node:http');
+const { createBareServer } = require('@tomphttp/bare-server-node');
+
+const httpServer = http.createServer();
+
+const bareServer = createBareServer('/bare/');
+
+httpServer.on('request', (req, res) => {
+	if (bareServer.shouldRoute(req)) {
+		bareServer.routeRequest(req, res);
+	} else {
+		res.writeHead(400);
+		res.end('Not found.');
+	}
+});
+
+httpServer.on('upgrade', (req, socket, head) => {
+	if (bareServer.shouldRoute(req)) {
+		bareServer.routeUpgrade(req, socket, head);
+	} else {
+		socket.end();
+	}
+});
+
+httpServer.on('listening', () => {
+	console.log('HTTP server listening');
+});
+
+httpServer.listen({
+	port: 3300,
+});
