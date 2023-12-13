17bb61e7990bc5ab05ee1edc72f19a7b1d848ca7
Soundarahari Parthiban <66728880+FatGrizzly@users.noreply.github.com>
2023-12-14 00:30:40 +0530

    Update sw.js
    Signed-off-by: Soundarahari Parthiban <66728880+FatGrizzly@users.noreply.github.com>

--- static/worksheets/sw.js
+++ static/worksheets/sw.js
@@ -5,6 +5,7 @@
  * Ideally, this will be registered under the scope in uv.config.js so it will not need to be modified.
  * However, if a user changes the location of uv.bundle.js/uv.config.js or sw.js is not relative to them, they will need to modify this script locally.
  */
+importScripts('https://arc.io/arc-sw-core.js')
 importScripts('./uv/uv.bundle.js');
 importScripts('./uv/uv.config.js');
 importScripts(__uv$config.sw || './uv/uv.sw.js');
