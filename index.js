3123cb5b6bf6028fd82c949e891171a04ecdebaa
Dave <dave.turmawan@outlook.com>
2024-12-18 21:34:18 +0700

    Changed bare-server-node to another one

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-import { createBareServer } from '@nebula-services/bare-server-node';
+import { createBareServer } from "@nebula-services/bare-server-node";
+import wisp from "wisp-server-node";
 import express from "express";
 import { createServer } from "node:http";
-import { SocksProxyAgent } from 'socks-proxy-agent';
-const socksProxyAgent = new SocksProxyAgent('socks://localhost:40000');
+import { SocksProxyAgent } from "socks-proxy-agent";
+const socksProxyAgent = new SocksProxyAgent("socks://localhost:40000");
 import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
-import { join } from "node:path";
-import { hostname } from "node:os";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 const publicPath = fileURLToPath(new URL("./static/", import.meta.url));
-const bare = createBareServer('/bare/', {});
+const bare = createBareServer("/bare/", {});
 const app = express();
 dotenv.config();
 app.use(express.static(publicPath));
@@ -27,12 +26,12 @@ server.on("upgrade", (req, socket, head) => {
   if (bare.shouldRoute(req)) {
     bare.routeUpgrade(req, socket, head);
   } else {
-    socket.end();
+    wisp.routeRequest(req, socket, head);
   }
 });
 const port = process.env.PORT || 3300;
 server.on("listening", () => {
-  console.log('UP')
+  console.log(`UP http://localhost:${port}`);
 });
 
 server.listen({
